a708034b99103f3619f11d092b086e95a376ca8a
K Truong <truong.k77@gmail.com>
2017-09-23 06:27:57 +0000

    update databse_setup.py

--- database_setup.py
+++ database_setup.py
@@ -1,14 +1,31 @@
 #!/usr/bin/python3
 
-# For environment variables
+import o
 import sys
 
-# SQLAlchemy Imports
+# SQLalchemy Imports
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 
-Base = declarative_base()
+# Instantiate declarative base
+Base =  declarative_base()
 
-engine = create_engine('sqlite///catalog.db')
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    username = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    
+
+class Cat(Base):
+    __tablename__ = 'cat'
+    user_id = Column(Integer, ForeignKey('user.id'))
+    name = Column(String(250))
+    image = Column (String(250))
+    description = Column(String(250))
+
+engine = create_engine('sqlite:///catalog.db')
+Base.metadata.create_all(engine)
