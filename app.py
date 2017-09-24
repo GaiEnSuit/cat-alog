f22a77f1dfeb07f48b0d40ab101926a92c9715af
K Truong <truong.k77@gmail.com>
2017-09-24 07:20:05 +0000

    combine database_setup, with populate, ran program to generate database, update import for sessionmaker

--- app.py
+++ app.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+
+# Flask setup
+from flask import Flask
+app = Flask(__name__)
+
+# SQlalchemy setup
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from database_setup import Base, User, Cat
+
+engine = create_engine('sqlite:///catalog.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+@app.route('/')
+def HelloWorld():
+    print 'Hello World'
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
--- database_setup.py
+++ database_setup.py
@@ -7,19 +7,21 @@ import sys
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
+
 
 # Instantiate declarative base
 Base = declarative_base()
 
-
+# table for users
 class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     email = Column(String(250), nullable=False)
 
-
+# table for cats
 class Cat(Base):
     __tablename__ = 'cat'
     id = Column(Integer, primary_key=True)
@@ -30,5 +32,40 @@ class Cat(Base):
     category = Column(String(250), nullable=False)
     user = relationship(User)
 
+# create databse
 engine = create_engine('sqlite:///catalog.db')
 Base.metadata.create_all(engine)
+
+# bind engine to database
+Base.metadata.bind = engine
+
+# session for initial data
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+# Add new user to database
+theCreator = User(name='The Creator',
+                  email='OmnipotentOne@gmail.com')
+
+session.add(theCreator)
+
+# Add new cat to databse
+round = Cat(name='Round',
+            image='https://sociorocketnewsen.files.wordpress.com/2013/10/maru-top.jpg',
+            description='He likes boxes',
+            category='Fat Cat',
+            user=theCreator)
+
+session.add(round)
+
+# Add second cat to databse
+wigglesborth = Cat(name='WigglesBorth',
+                   image='http://www.cinemacats.com/wp-content/uploads/movies/austinpowersint05.jpg',
+                   description='Cat of Dr No So Evil',
+                   category='Hairless Cat',
+                   user=theCreator)
+
+session.add(wigglesborth)
+
+# commit changes to databse
+session.commit()
